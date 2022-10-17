1c99f11187e381283e319e534bbc0d2321413c8a
bcorey <benjaminmcorey@gmail.com>
2022-10-17 13:04:47 -0400

    responsive image viewer

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,107 @@
 use dioxus::prelude::*;
 use dioxus::events::{MouseEvent, FormEvent};
+use img_render::run;
+use img_render::WebImage;
+use img_render::FrontendEvent;
+use winit::event_loop::{EventLoopProxy};
+// use image::DynamicImage;
+
+
+use wasm_logger;
+
+mod image_decode;
+use image_decode::{get_file, manual_decode, canvas_decode, save_canvas};
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
     dioxus::web::launch(app);
 }
 
-fn app(cx: Scope) -> Element {
-    let mut value = use_state(&cx, || 0);
+fn dims(id: &str) -> (f64, f64) {
+    let parent_rect = web_sys::window()
+    .unwrap().document()
+    .unwrap().get_element_by_id(id)
+    .unwrap().get_bounding_client_rect();
+    (parent_rect.width(), parent_rect.height())
+}
 
-    cx.render(rsx!{    
+fn prepare_img(cx: Scope, proxy: &Option<EventLoopProxy<FrontendEvent>>) {
+    cx.spawn({
+        let proxy_2 = proxy.clone();
+        async move {
+            if let Some(file) = get_file("img") {
+                let n = canvas_decode(file).await;
+                match n {
+                    Some(img) => send_img(&proxy_2, img),
+                    _ => ()
+                };
+            }
+        }
+    })
+}
+
+fn send_shader_event(cx: Scope, event: FrontendEvent) {
+    let proxy = use_read(&cx, PROXY);
+
+    if let Some(proxy) = proxy {
+        proxy.send_event(event);
+    }
+}
+
+fn send_img(proxy: &Option<EventLoopProxy<FrontendEvent>>, img: WebImage) {
+    if let Some(proxy) = proxy {
+        proxy.send_event(FrontendEvent::NewImage(img));
+    }
+}
+
+static PROXY: Atom<Option<EventLoopProxy<FrontendEvent>>> = |_| None;
+static PIPELINE_STATUS: Atom<bool> = |_| false;
+static INITIAL_PAGE_LOAD: Atom<bool> = |_| false;
+static VALUE: Atom<i32> = |_| 0;
+
+fn start(cx: Scope) {
+    let pipeline_status = use_read(&cx, PIPELINE_STATUS);
+    let set_pipeline_status = use_set(&cx, PIPELINE_STATUS);
+    let set_proxy = use_set(&cx, PROXY);
+    let initial_load = use_read(&cx, INITIAL_PAGE_LOAD);
+    let set_initial_load = use_set(&cx, INITIAL_PAGE_LOAD);
+
+
+    if !pipeline_status && *initial_load {
+        let (view_width, view_height) = dims("parent");
+        set_proxy(Some(run(view_width, view_height)));
+        set_pipeline_status(true);
+    } 
+
+    if !initial_load {
+        set_initial_load(true);
+    }
+}
+
+fn app(cx: Scope) -> Element {
+    //let mut value = use_state(&cx, || 0);
+    let value = use_read(&cx, VALUE);
+    let set_value = use_set(&cx, VALUE);
+    let proxy = use_read(&cx, PROXY);
+
+    log::info!("updating ui");
+    start(cx);
+
+    cx.render(rsx!{ 
+        DecodeCanvas {}
+        DownloadAnchor {}
+        div {
+            class: "row row-center",
+            Canvas {}
+        }  
+
         div{
             class: "container",
             div {
                 class: "row row-center",
-                h3 { 
-                    "hello dioxus!"
-                }
-            }
-            div {
-                class: "row row-center",
                 VoteButton {
                     name: "+",
-                    onclick: move |_| value += 1,
+                    onclick: move |_| set_value(value + 1),
                 }
                 div {
                     class: "column column-25",
@@ -33,18 +113,30 @@ fn app(cx: Scope) -> Element {
                 }
                 VoteButton {
                     name: "-",
-                    onclick: move |_| value -= 1,
-                }
-                FileInput {
-                    file_types: "image",
-                    id: "img",
-                    oninput: move |_| value += 100,
+                    onclick: move |_| set_value(value - 1),
                 }
             }
             div {
                 class: "row row-center",
-                Canvas {}
+                FileInput {
+                    file_types: "image",
+                    id: "img",
+                    oninput: move |_| prepare_img(cx, proxy),
+                }
+                VoteButton {
+                    name: "Step",
+                    onclick: move |_| send_shader_event(cx, FrontendEvent::STEP),
+                }
+                VoteButton {
+                    name: "Fill Mode",
+                    onclick: move |_| send_shader_event(cx, FrontendEvent::FILL_MODE),
+                }
+                VoteButton {
+                    name: "Save",
+                    onclick: move |_| save_canvas(),
+                }
             }
+
         }
     })
 }
@@ -72,10 +164,33 @@ fn VoteButton<'a>(cx: Scope<'a,VoteButtonProps<'a>>) -> Element {
 fn Canvas(cx: Scope) -> Element {
     cx.render(rsx!{
         div {
-            class: "column column-100",
+            class: "",
+            id: "parent",
             canvas {
-                id: "drawing-canvas",
+                id: "canvas",
+                prevent_default: "onclick",
+                onclick: move |_|{
+                    // handle the event without navigating the page.
+                }
             }
+        }
+    })
+}
+
+fn DecodeCanvas(cx: Scope) -> Element {
+    cx.render(rsx!{
+        canvas {
+            id: "decode-canvas",
+            style: "display: none"
+        }
+    })
+}
+
+fn DownloadAnchor(cx: Scope) -> Element {
+    cx.render(rsx!{
+        a {
+            id: "download-anchor",
+            style: "display: none"
         }
     })
 }
